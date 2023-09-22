44edaff45654b8dd2a594831d939d0008fdbe940
Simon Walker <simon.walker@localstack.cloud>
2023-09-22 16:37:02 +0100

    Add ssm parameters and enqueue setup

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import uuid
 from functools import lru_cache
-import os
+import json
 
 from flask import Flask, request, render_template, redirect, flash, url_for
 import boto3
@@ -8,17 +8,21 @@ import boto3
 app = Flask(__name__)
 app.secret_key = "secret-key"
 
-BUCKET_NAME = os.environ["BUCKET_NAME"]
-QUEUE_URL = os.environ["QUEUE_URL"]
-
 
 @lru_cache
 def client_factory(service: str):
     return boto3.client(service, endpoint_url="http://localhost.localstack.cloud:4566")
 
 
+ssm_client = client_factory("ssm")
 s3_client = client_factory("s3")
 sqs_client = client_factory("sqs")
+
+# Set up constants
+raw_ssm_parameters = ssm_client.get_parameters_by_path(Path="/network-demo", Recursive=True)[
+    "Parameters"
+]
+ssm_parameters = {every["Name"]: every["Value"] for every in raw_ssm_parameters}
 
 
 @app.route("/")
@@ -36,9 +40,10 @@ def handle_upload():
         # TODO: secure filename
         file_id = str(uuid.uuid4())
         s3_key = f"raw/{file_id}"
-        s3_client.upload_fileobj(file, BUCKET_NAME, s3_key)
+        s3_client.upload_fileobj(file, ssm_parameters["/network-demo/bucket"], s3_key)
 
         # enqueue message
+        sqs_client.send_message(QueueUrl=ssm_parameters["/network-demo/queue"], MessageBody=json.dumps({}))
 
         flash("File uploaded!")
         return redirect(url_for("index"))
