42896fa8123d767a208efeb8349cccee22521173
Simon Walker <simon.walker@localstack.cloud>
2023-09-22 18:00:10 +0100

    Handle (stub) file size calculation

--- cdk/lib/cdk-stack.Handler.ts
+++ cdk/lib/cdk-stack.Handler.ts
@@ -3,5 +3,27 @@ import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 
 
 export const handler: SQSHandler = async (event: SQSEvent) => {
-  console.log(event);
+  const client = new DynamoDBClient({ region: "us-east-1", endpoint: "http://localhost.localstack.cloud:4566", });
+
+  for (const record of event.Records) {
+    const { id, bucket, filename } = JSON.parse(record.body);
+    console.log(id, bucket, filename);
+    const n = 10;
+    // insert into the database
+    const command = new PutItemCommand({
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        pk: { S: id },
+        bucket: { S: bucket },
+        filename: { S: filename },
+        size: { N: n.toString() },
+      },
+    });
+
+    try {
+      await client.send(command);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
