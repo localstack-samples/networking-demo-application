f6b80a51e97fb29c271bf82d51ab9095bba9a9b4
Simon Walker <s.r.walker101@googlemail.com>
2024-02-26 22:42:18 +0000

    Configure clients via envars

--- app.py
+++ app.py
@@ -12,11 +12,7 @@ app.secret_key = "secret-key"
 
 @lru_cache
 def client_factory(service: str):
-    return boto3.client(
-        service,
-        endpoint_url="http://localhost.localstack.cloud:4566",
-        region_name="us-east-1",
-    )
+    return boto3.client(service)
 
 
 ssm_client = client_factory("ssm")
